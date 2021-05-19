26a7e46f1b19e997f18aa554bc015da9438ba52a
MYurkevich <mihailyur1527@gmail.com>
2021-05-19 17:54:15 +0500

    Приступил к адаптиву хеадера

--- JS/script.js
+++ JS/script.js
@@ -27,3 +27,17 @@ document.getElementById('searchBar').onclick = function() {
     document.getElementById('searchBar').classList.toggle('search-icon--open');
 }
 // --------END добавление класса для поиска-------
+
+
+//START Бургер
+const iconMenu = document.querySelector('.menu__icon') //получаем иконку
+const menuBody = document.querySelector('.header__navigation') //получаем контент
+
+if (iconMenu){ //Условие, если имеется такой класс
+    iconMenu.addEventListener("click", function (e) { //добавляем событие клика на иконку
+        iconMenu.classList.toggle('_active');   //добавляем класс иконке, чтобы можно было с ней взаимодействовать
+        menuBody.classList.toggle('_active');   //добавляем класс контенту
+        document.body.classList.toggle('_lock'); //Добавление класса body, чтобы при появлении контента нельзя было прокручивать основной контент
+    });
+}
+//END Бургер
